b6ac6ff052de11d6d5999261f04117cb331dc44d
Zainab Sansa <tunrayotemitope05@gmail.com>
2024-01-12 22:29:49 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+//JavaScript Can Change HTML Styles (CSS)
+document.getElementById("demo").style.fontSize = "35px";
+
+// JavaScript Can Hide HTML Elements
+document.getElementById("demo").style.display = "none"; 
+
+// JavaScript Can Show HTML Elements
+document.getElementById("demo").style.display = "block"; 
+
+//In HTML, JavaScript code is inserted between //<script> and </script> tags.
+// You can place any number of scripts in an HTML document.
+// Scripts can be placed in the <body>, or in the <head> section of an HTML page, or in both.
